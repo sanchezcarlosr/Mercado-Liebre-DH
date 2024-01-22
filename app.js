ea303ebee7236cc2ecc5d28e43dad28c6444f553
Carlos Sanchez <argcarlos688@gmail.com>
2024-01-22 16:21:51 -0300

    uploagind settings to render

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('node:path');
 
 const app = express();
 
-app.listen(3000, ()=> console.log("Server running on port 3000"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`));
 
 app.get('/', (req, res) =>{
 	res.sendFile(path.join(__dirname, "/views/home.html"));
